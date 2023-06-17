5a92d2ffa70f534bae5fb4f4c6b49a19286ab2f2
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-17 14:18:44 +0100

    Update cool.lua

--- cool.lua
+++ cool.lua
@@ -1,6 +1,1 @@
 _G.ids = {}
-
-table.insert(_G.ids,"999") -- 720935327791054848
-table.insert(_G.ids,"99") -- 720935327791054848
-table.insert(_G.ids,"99") -- 720935327791054848
-table.insert(_G.ids,"99") -- 720935327791054848
