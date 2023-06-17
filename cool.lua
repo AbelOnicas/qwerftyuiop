fca9f4c78ef0ffbbb18b8142aa96494a4c3f4ed5
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-17 23:01:56 +0100

    Update file contents

--- cool.lua
+++ cool.lua
@@ -3,3 +3,4 @@ _G.qwertyuiopasdfghjklzxcvbnm = {}
 table.insert(_G.cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0=, VGVzdA==) -- 720935327791054848
 table.insert(_G.cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0=, cmVhbA==) -- 1053080794039591043
 table.insert(_G.cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0=, MzA5ZDZkM2UtOWYwMC00MTY3LThjNDEtMTE4MTI3NmE3ZDMx) -- 1053080794039591043
+table.insert(_G.cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0=, MzA5ZDZkM2UtOWYwMC00MTY3LThjNDEtMTE4MTI3NmE3ZDMx) -- 1053080794039591043
