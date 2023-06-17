cc5f70d722be18988fb67645c52252765873506d
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-17 14:11:13 +0100

    Update file contents

--- cool.lua
+++ cool.lua
@@ -5,3 +5,4 @@ table.insert(_G.ids,"Test") -- 720935327791054848
 table.insert(_G.ids,"Good") -- 720935327791054848
 table.insert(_G.ids,"2") -- 720935327791054848
 table.insert(_G.ids,"9") -- 720935327791054848
+table.insert(_G.ids,"10") -- 720935327791054848
