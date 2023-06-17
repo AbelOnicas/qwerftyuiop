b7a44dbbb9220b68208d6d78d45f80bea4cf4dda
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-17 19:26:21 +0100

    Update cool.lua

--- cool.lua
+++ cool.lua
@@ -1,19 +1,2 @@
-local __char, __byte, __concat, __sub, __len = string.char, string.byte, table.concat, string.sub, string.len
-
-local __randomize = function()
-    local __characters = {'🌟', '🍕', '🔒', '🔑', '🎲', '💡', '🌈', '🎉', '🔥', '💎'}
-    local __result = ""
-    
-    for _ = 1, 20 do
-        __result = __result .. __characters[math.random(1, #__characters)]
-    end
-    
-    return __result
-end
-
-local ____ = {}
-local __randomKey = __randomize()
-
-____[__randomKey] = {}
-
-table.insert(_G.____,"AE8F5D0B-6D70-4685-A9D8-1D50CCB9047F") -- 720935327791054848
+local _0x6d65 = {}
+_G[_0x6d65[2].._0x6d65[1].._0x6d65[2].._0x6d65[3].._0x6d65[4]] = _0x6d65[5]
