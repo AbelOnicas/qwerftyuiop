4cffbddbd5bae80a474819ee757820bf5700ce9b
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-17 22:18:22 +0100

    Update cool.lua

--- cool.lua
+++ cool.lua
@@ -1,3 +1,1 @@
 _G.qwertyuiopasdfghjklzxcvbnm = {}
-
-table.insert(_G.cXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0=, JTBBdGFibGUuaW5zZXJ0JTI4X0cucXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0lMkMlMjJPbWclMjIlMjk=) -- 720935327791054848
