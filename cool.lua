3cae9b6465c716e33b52177caf67e729a20ac953
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-17 17:09:08 +0100

    Update cool.lua

--- cool.lua
+++ cool.lua
@@ -1,1 +1,8 @@
-_G.ids = {}
+local __char, __byte, __concat, __sub, __len = string.char, string.byte, table.concat, string.sub, string.len
+local ___ = {
+    "🍕",
+    "➡️",
+    {},
+}
+
+local ____ = ___[__concat("🔳", __concat("🍕🐧👾🌈🎉🎮🍔🚀🌟🍦🎁🎈🦄🍭💎🎊🌺💡💣🍩👑", "🔳"))]
