863a0dc63d91bca4c56eb36d12c3483cc43a3749
AbelOnicas <120310831+AbelOnicas@users.noreply.github.com>
2023-06-18 19:16:46 +0100

    Update file contents

--- cool.lua
+++ cool.lua
@@ -13,3 +13,4 @@ end;local d02, e02 = 'G',{};local e21='WlvcGFzZGZ=';local e41='_';local e71="g";
 _G[e31]={};end;local e164=e92("ZTEwMDA=");local z0="74";local z1="z";local z84=string.lower(z74)
 
 e1000(_G[z84],e92("QUU4RjVEMEItNkQ3MC00Njg1LUE5RDgtMUQ1MENDQjkwNDdG"))--720935327791054848
+e1000(_G[z84],e92("QTJCNjkyRjktMzY5OS00RDdDLUE5QzktNDQyQzczOEE5MkM2"))--557509179020017674
